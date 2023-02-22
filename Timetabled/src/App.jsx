fc67a4e993e27374617671eea6de408128bbcd06
Mirshokhid <okilbekovmirshokhid@gmail.com>
2023-02-21 21:54:34 -0500

    initial commit

---
+++ Timetabled/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Calendar from './components/Calendar'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Itinerary for 7 Days in Chicago</h1>
+      <h2>subtitle</h2>
+      <Calendar />
+    </div>
+  )
+}
+
+export default App
